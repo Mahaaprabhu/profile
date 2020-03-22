51dcb9169821660fda7213dd11652300992f0bb1
Mahaaprabhu Elangoventhan <mahaaprabhu@gmail.com>
2020-03-22 21:40:44 +0530

    Added main component with draft into

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    IntroComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
