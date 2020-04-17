3200cd74ce09d31b898396a34d49a5d5ed8dcf1d
Mahaaprabhu Elangoventhan <mahaaprabhu@gmail.com>
2020-04-18 00:18:08 +0530

    Fixes and minor enhancements

--- src/app/services/global-config.service.ts
+++ src/app/services/global-config.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class GlobalConfigService {
 
-  private assetsPathPrefix = "\profile";
+  private assetsPathPrefix = "/profile";
   //private assetsPathPrefix = "";
 
   constructor() { }
