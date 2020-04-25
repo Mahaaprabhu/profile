a0bc1798e1c6dbfd3f5a61f1d535536054fbcafa
Mahaaprabhu Elangoventhan <mahaaprabhu@gmail.com>
2020-04-25 22:21:28 +0530

    Logic for clipboard copy

--- src/app/components/popup/popup.component.ts
+++ src/app/components/popup/popup.component.ts
@@ -17,6 +17,10 @@ export class PopupComponent implements OnInit {
   @Input()
   displayPopup: boolean = false;
 
+  displayTooltip: boolean = false;
+  displaySpecialColor: boolean = false;
+  tooltip: string = '';
+
   constructor(private appStateService: AppStateService) { 
 
   }
@@ -41,6 +45,36 @@ export class PopupComponent implements OnInit {
     this.displayPopup = false;
     this.popupData = '';
     this.popupType = '';
+    this.tooltip = '';
+  }
+
+  public onContentCopy(): void  {
+    this.onDisplayToolTip();
+    this.executeClipboardCopy();
+  }
+
+  private onDisplayToolTip(): void {
+    this.tooltip = `Coppied '${this.popupData}'`;
+    this.displayTooltip = true;
+    this.displaySpecialColor = true;
+    setTimeout(()=>{
+      this.displayTooltip = false;
+      this.displaySpecialColor = false;
+      this.tooltip = '';
+    }, 1000);
+  }
+
+  private executeClipboardCopy(): void {
+    const dynamicElement: HTMLTextAreaElement = document.createElement('textarea');
+    dynamicElement.value = this.popupData;
+    dynamicElement.style.height = '0';
+    dynamicElement.style.width = '0';
+    dynamicElement.style.opacity = '0';
+    document.body.appendChild(dynamicElement);
+    dynamicElement.focus();
+    dynamicElement.select();
+    document.execCommand('copy');
+    document.body.removeChild(dynamicElement);
   }
 
 }
